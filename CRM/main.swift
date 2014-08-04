585ee22c68b8d3ba1e4a6f294940c3d08413fe2b
Roman <prociona@yandex.ru>
2014-08-04 22:49:36 +0400

    Initial Commit

---
+++ CRM/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  CRM
+//
+//  Created by Roman Bolshakov on 2014/08/04.
+//  Copyright (c) 2014年 Roman Bolshakov. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
